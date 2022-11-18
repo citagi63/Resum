65bc7119e5f0a449992387f956f9c98347922deb
citagi63 <103110800+citagi63@users.noreply.github.com>
2022-11-18 23:45:13 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -36,13 +36,13 @@ variable "vpc_cidr_block" {
 
 variable "private_subnet_cidr_blocks" {
   type        = list(string)
-  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+  default     = ["10.0.6.0/24", "10.0.4.0/24"]
   description = "CIDR block range for the private subnets"
 }
 
 variable "public_subnet_cidr_blocks" {
   type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+  default     = ["10.0.1.0/24", "10.0.3.0/24"]
   description = "CIDR block range for the public subnets"
 }
 
@@ -65,5 +65,5 @@ variable "number_of_private_subnets_db"{
 }
 variable "private_subnet_cidr_blocks_db"{
   type = list(string)
-  default = ["10.0.5.0/24", "10.0.6.0/24"]
+  default = ["10.0.5.0/24", "10.0.7.0/24"]
   }
