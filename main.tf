75719e4417b583b1f263874f64cf429e4451c489
citagi63 <103110800+citagi63@users.noreply.github.com>
2022-11-21 20:19:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -70,12 +70,12 @@ resource "aws_route_table" "private_route_table" {
 }
 resource "aws_route_table_association" "igw_public_subnet_assoc" {
   count = var.number_of_private_subnets
-  route_table_id = aws_route_table.public_route_table.id
+  route_table_id = aws_route_table.public_route_table[count.index].id
   subnet_id = aws_subnet.conductor_public_subnet[count.index].id 
   }
 resource "aws_route_table_association" "nat_private_subnet_assoc" {
   count = var.number_of_private_subnets
-  route_table_id = aws_route_table.private_route_table.id
+  route_table_id = aws_route_table.private_route_table[count.index].id
   subnet_id = [ aws_subnet.conductor_private_subnet[count.index].id , aws_subnet.conductor_private_subnet_db ]
   }
   resource "aws_route" "ig_public_subnet_route" {
