51834e998c3b3933a39aaf7935553fe9300e9355
citagi63 <103110800+citagi63@users.noreply.github.com>
2022-11-21 17:36:21 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -57,4 +57,8 @@ variable "internal_user_database_enabled" {
     type       = bool
     description = "internal user for database"
 }
+variable "subnet_ids" {
+    type       = list(string)
+    description = "internal user for database"
+}
 
