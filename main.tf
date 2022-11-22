da7ec727c1b4766392f39a983823a0017ad88dcf
citagi63 <103110800+citagi63@users.noreply.github.com>
2022-11-22 14:51:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -41,59 +41,25 @@ resource "aws_internet_gateway" "internet_gateway" {
   vpc_id = aws_vpc.conductor_vpc.id
 }
 resource "aws_eip" "elastic_ip" {
-  count = var.number_of_public_subnets
   vpc = true
   tags = {
-    Name = "elastic_ip-${count.index + 1}"
-  }
-}
-resource "aws_nat_gateway" "conductor_nat" {
-    allocation_id = aws_eip.elastic_ip[count.index].id
-  count = var.number_of_public_subnets
-  subnet_id = aws_subnet.conductor_public_subnet[count.index].id
-  tags = {
-    Name = "nat_gateway-${var.environment}"
-  }
-}
-resource "aws_route_table" "public_route_table" {
-  vpc_id = aws_vpc.conductor_vpc.id
-  count = var.number_of_public_subnets
-  tags = {
-    Name = "${aws_subnet.conductor_private_subnet[count.index].availability_zone}-route-table-public-${var.environment}"
-  }
-}
-resource "aws_route_table" "private_route_table" {
-  vpc_id = aws_vpc.conductor_vpc.id
-  count = var.number_of_private_subnets
-  tags = {
-    Name = "${aws_subnet.conductor_private_subnet[count.index].availability_zone}-route-table-NAT-${var.environment}"
+    Name = "elastic_ip-dev"
   }
 }
 
-resource "aws_route_table_association" "nat_private_subnet_db" {
-  count = var.number_of_private_subnets_db
-  route_table_id = aws_route_table.private_route_table[count.index].id
-  subnet_id = aws_subnet.conductor_private_subnet_db[count.index].id
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.conductor_vpc.id
+  tags = {
+    Name = "public-route-table-${availability_zone}-route-table-public-${var.environment}"
   }
+}
 resource "aws_route_table_association" "igw_public_subnet_assoc" {
-  count = var.number_of_private_subnets
   route_table_id = aws_route_table.public_route_table[count.index].id
   subnet_id = aws_subnet.conductor_public_subnet[count.index].id 
   }
-resource "aws_route_table_association" "nat_private_subnet_assoc" {
-  count = var.number_of_private_subnets
-  route_table_id = aws_route_table.private_route_table[count.index].id
-  subnet_id = aws_subnet.conductor_private_subnet[count.index].id
-  }
   resource "aws_route" "ig_public_subnet_route" {
-  count = var.number_of_public_subnets
-  route_table_id = aws_route_table.public_route_table[count.index].id
+  route_table_id = aws_route_table.public_route_table.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id = aws_internet_gateway.internet_gateway.id
 }
-resource "aws_route" "nat_private_subnet_route" {
-  count = var.number_of_private_subnets
-  route_table_id = aws_route_table.private_route_table[count.index].id
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id = aws_nat_gateway.conductor_nat[count.index].id
-}
+
