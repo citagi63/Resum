2ea63e5215bc3b89246412160bcb587e9bb003bd
citagi63 <103110800+citagi63@users.noreply.github.com>
2022-11-22 10:44:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "aws_elasticsearch_domain" "opensearch" {
   
   vpc_options {
   subnet_ids = [
-   "${element(data.aws_subnet_ids.conductor_private_subnet_dev.ids, count.index)}"
+   "${element(data.aws_subnet_ids.value.conductor_private_subnet_dev.ids, count.index)}"
    ]
    security_group_ids = [""]
 
