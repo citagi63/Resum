cce9aae965a4da0aefad5c83e505361604eb739b
citagi63 <103110800+citagi63@users.noreply.github.com>
2022-11-23 15:36:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "allow_alb" {
   name        = "allow_alb"
   description = "Allow Alb inbound traffic"
-  vpc_id      = aws_vpc.main.id
+  vpc_id      = var.vpc_id
 
   ingress {
     description      = "Alb from ecs_cluster"
