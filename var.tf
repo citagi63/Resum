22bd99e442c042abce20e723cca81641045570b2
citagi63 <103110800+citagi63@users.noreply.github.com>
2022-11-23 16:32:48 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -23,11 +23,11 @@ variable "vpc_id" {
 }
 variable "ingress_rules" {
   default     = {
-    "description" = ["For HTTP", "For SSH"]
-    "from_port"   = ["80", "5000"]
-    "to_port"     = ["80", "22"]
-    "protocol"    = ["tcp", "tcp"]
-    "cidr_blocks" = ["0.0.0.0/0", "0.0.0.0/0"]
+    "description" = ["For HTTP", "For conductor", "For Api"]
+    "from_port"   = ["80", "5000", "8080"]
+    "to_port"     = ["80", "5000", "8080"]
+    "protocol"    = ["tcp", "tcp", "tcp"]
+    "cidr_blocks" = ["0.0.0.0/0", "0.0.0.0/0" "0.0.0.0/0"]
   }
   type        = map(list(string))
   description = "Security group rules"
