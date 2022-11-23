290bf4f0cd37d34c6354858ae2204738a856c902
citagi63 <103110800+citagi63@users.noreply.github.com>
2022-11-23 15:40:37 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -5,3 +5,11 @@ output "cluster_version" {
   description = "The version of the OpenSearch cluster."
   value       = replace(aws_elasticsearch_domain.opensearch.elasticsearch_version, "OpenSearch_", "")
 }
+output "cluster_endpoint" {
+  description = "The endpoint URL of the OpenSearch cluster."
+  value       = "https://${aws_elasticsearch_domain.opensearch.domain_endpoint_options[0].custom_endpoint}"
+}
+output "kibana_endpoint" {
+  description = "The endpoint URL of the OpenSearch dashboards."
+  value       = "https://${aws_elasticsearch_domain.opensearch.domain_endpoint_options[0].custom_endpoint}/_dashboards/"
+}
