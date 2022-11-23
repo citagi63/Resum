71b6a5bae1d33d1256cbff74dea3dd09c6fabae8
citagi63 <103110800+citagi63@users.noreply.github.com>
2022-11-23 10:23:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,26 @@
 resource "aws_ecs_cluster" "conductor" {
     name = var.cluster_name
 }
+resource "aws_ecr_repository" "ecr_repo"{
+    name= var.ecr_repo
+}
+resource "aws_task_definition" "conductor_task" {
+    family = var.ecr_repo
+    network_mode             = "awsvpc"
+    requires_compatibilities = ["FARGATE"]
+    cpu                      = 256
+    memory                   = 512
+    execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+    task_role_arn            = aws_iam_role.ecs_task_role.arn
+    container_definitions = jsonencode([{
+    name        = "${var.cluster_name}-container-${var.environment}"
+    image       = "ngnix:latest"
+    essential   = true
+    environment = var.container_environment
+    portMappings = [{
+        protocol      = "tcp"
+        containerPort = var.container_port
+        hostPort      = var.container_port
+   }]
+}
+}
