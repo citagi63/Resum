d284c18480198e7bcb4216d30e00a64e47cb4509
citagi63 <103110800+citagi63@users.noreply.github.com>
2022-11-23 11:03:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,6 @@
 resource "aws_ecs_cluster" "conductor" {
     name = var.cluster_name
 }
-resource "aws_ecs_cluster" "conductor" {
-    name = var.cluster_name
-}
 resource "aws_iam_role" "ecs_exec_role"{
     name = "task_execution_role"
     assume_role_policy = jsonencode({ 
@@ -24,7 +21,6 @@ resource "aws_iam_role" "ecs_exec_role"{
     ]
 })
 }
-
 resource "aws_ecr_repository" "ecr_repo"{
     name= var.ecr_repo
 }
@@ -48,26 +44,3 @@ resource "aws_task_definition" "conductor_task" {
    }]
 }
 }
-resource "aws_ecr_repository" "ecr_repo"{
-    name= var.ecr_repo
-}
-resource "aws_ecs_task_definition" "conductor_task" {
-    family = var.ecr_repo
-    network_mode             = "awsvpc"
-    requires_compatibilities = ["FARGATE"]
-    cpu                      = 256
-    memory                   = 512
-    execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
-    task_role_arn            = aws_iam_role.ecs_task_role.arn
-    container_definitions = jsonencode([{
-    name        = "${var.cluster_name}-container-${var.environment}"
-    image       = "ngnix:latest"
-    essential   = true
-    environment = [{"name": "VARNAME", "value": var.environment}]
-    portMappings = [{
-        protocol      = "tcp"
-        containerPort = var.container_port
-        hostPort      = var.container_port
-   }]
-}])
-        }
