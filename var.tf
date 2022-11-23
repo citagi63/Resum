66e48ecf39c9a0e2f796627e94d1fcbab375d73a
citagi63 <103110800+citagi63@users.noreply.github.com>
2022-11-23 18:00:55 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -17,18 +17,12 @@ variable "environment" {
 variable "private_subnet_ids" {
     type = list(string)
 }
+
 variable region {}
+
 variable "vpc_id" {
     type = string
 }
-variable "ingress_rules" {
-  default     = {
-    "description" = ["For HTTP", "For conductor", "For Api"]
-    "from_port"   = ["80", "5000", "8080"]
-    "to_port"     = ["80", "5000", "8080"]
-    "protocol"    = ["tcp", "tcp", "tcp"]
-    "cidr_blocks" = ["0.0.0.0/0", "0.0.0.0/0", "0.0.0.0/0"]
-  }
-  type        = map(list(string))
-  description = "Security group rules"
+variable alb_name {
+  type  = string 
 }
