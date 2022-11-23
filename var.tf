5a87e7b24a350a6f40bee9f05f673922e25458fe
citagi63 <103110800+citagi63@users.noreply.github.com>
2022-11-23 10:24:11 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -3,3 +3,14 @@ variable "cluster_name" {
   default     = "conductor_cluster"
   description = "Name the cluster"
 }
+variable "ecr_repo" {
+    type = string
+    default = "conductor-img"
+}
+variable "container_port" {
+    type = number
+    default = 80
+}
+variable "environment" {
+    type = string
+}
